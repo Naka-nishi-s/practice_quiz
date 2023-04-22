754154c792f1657da6ca054326299b55d5120d3d
Hyodo <90432430+Naka-nishi-s@users.noreply.github.com>
2023-04-22 17:31:23 +0900

    first_commit

--- practice_quiz/src/App.tsx
+++ practice_quiz/src/App.tsx
@@ -1,7 +1,35 @@
+import { useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
+
+  // データ取得用の関数
+  const showData = () => {
+    // データ取得
+    axios
+      .get("/user")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((e) => {
+        console.log("エラーが出てます。");
+        console.log(e);
+      });
+  };
+
+  // 初期データ取得
+  useEffect(() => {
+    console.log("Nice Judge!!");
+    showData();
+  }, []);
+
+  return (
+    <div className="App">
+      <button onClick={showData}>Click!!</button>
+    </div>
+  );
+};
 
 export default App;
